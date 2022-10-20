2001421a2fc4060c921e97618c4c349eb7c5e9c8
Arit Chanda <aritchanda1234@gmail.com>
2022-10-20 00:04:24 -0700

    Added Error Handling and Toasts for Encryptor

--- frontend/src/app/components/encryptor/encryptor.component.ts
+++ frontend/src/app/components/encryptor/encryptor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ApiserviceService } from 'src/app/services/apiservice.service';
 import { environment } from 'src/environments/environment';
 
@@ -11,12 +12,15 @@ import { environment } from 'src/environments/environment';
 export class EncryptorComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
-    private api: ApiserviceService
+    private api: ApiserviceService,
+    private router: Router
   ) {
     this.isLoading = false;
     this.isEncrypted = false;
     this.password = '';
     this.link = '';
+    this.errorMessage = '';
+    this.isError = false;
   }
 
   encryptionForm!: FormGroup;
@@ -24,6 +28,8 @@ export class EncryptorComponent implements OnInit {
   isEncrypted!: boolean;
   password!: string;
   link!: string;
+  isError!: boolean;
+  errorMessage!: string;
 
   ngOnInit(): void {
     this.encryptionForm = this.formBuilder.group({
@@ -42,17 +48,23 @@ export class EncryptorComponent implements OnInit {
       let { password, text, viewCount } = this.encryptionForm.value;
       this.api.encrypt(text, password, viewCount).subscribe({
         next: (res) => {
-          console.log(res);
           this.isLoading = false;
           this.isEncrypted = true;
           this.password = res.password;
           this.link = res.link;
         },
         error: (err) => {
-          console.log('weoe', err);
           this.isLoading = false;
+          if (err.status === 500) this.router.navigateByUrl('/500');
+          this.isError = true;
+          this.errorMessage = err.error.toString();
         },
       });
+    } else {
+      console.log(this.isError, 'adsasadasdsa');
+      this.isError = true;
+      this.errorMessage = 'Form Data is Invalid';
+      this.isLoading = false;
     }
   }
 
@@ -61,5 +73,10 @@ export class EncryptorComponent implements OnInit {
     this.password = '';
     this.link = '';
     this.encryptionForm.reset();
+  }
+
+  toastHidden() {
+    this.isError = false;
+    this.errorMessage = '';
   }
 }
