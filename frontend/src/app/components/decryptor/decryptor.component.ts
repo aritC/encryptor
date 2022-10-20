0f1e44d367e17af127931a4a34914b055622c5f9
Arit Chanda <aritchanda1234@gmail.com>
2022-10-20 00:03:53 -0700

    Added functionality for decryptor

--- frontend/src/app/components/decryptor/decryptor.component.ts
+++ frontend/src/app/components/decryptor/decryptor.component.ts
@@ -1,15 +1,73 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ApiserviceService } from 'src/app/services/apiservice.service';
+
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-decryptor',
   templateUrl: './decryptor.component.html',
-  styleUrls: ['./decryptor.component.css']
+  styleUrls: ['./decryptor.component.css'],
 })
 export class DecryptorComponent implements OnInit {
-
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private api: ApiserviceService,
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.uid = '';
+    this.isLoading = false;
+    this.isDecrypted = false;
+    this.decryptionForm = this.formBuilder.group({
+      password: ['', Validators.required],
+    });
+    this.isError = false;
+  }
+  decryptionForm!: FormGroup;
+  uid!: string;
+  isExpired!: boolean;
+  isLoading!: boolean;
+  isDecrypted!: boolean;
+  decryptedText!: string;
+  isError!: boolean;
 
   ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      this.uid = params.get('uid')!;
+      this.api.checkLink(this.uid).subscribe({
+        next: (res) => {
+          this.isExpired = false;
+        },
+        error: (err) => {
+          if (err.status === 400) {
+            this.isExpired = true;
+          } else {
+            this.router.navigateByUrl('/500');
+          }
+        },
+      });
+    });
   }
 
+  decrypt(): void {
+    this.isLoading = true;
+    if (this.decryptionForm.valid) {
+      let { password } = this.decryptionForm.value;
+      this.api.decrypt(password, this.uid).subscribe({
+        next: (res) => {
+          this.isDecrypted = true;
+          this.isLoading = false;
+          this.decryptedText = res.decryptedText;
+          console.log(res);
+        },
+        error: (err) => {
+          this.isLoading = false;
+          this.isError = true;
+          if (err.status === 500) this.router.navigateByUrl('/500');
+        },
+      });
+    }
+  }
 }
